bcf0d528eaba7ff6b32a3e58c5e9c67bc1f9360e
Maria Sottile <31717619+masottile@users.noreply.github.com>
2021-03-30 19:34:50 -0700

    improve one time step, brainstorm geography

--- main.pl
+++ main.pl
@@ -1,24 +1,6 @@
-% A playground for testing out different Natural Selection Code
+% Actual code for parts of the project
 
-% Rate Equation Based NS -----------------------------------------------------------------
-% entity_traits(type_of_entity, reproduction_probability, death_probability)
-entity_traits(blob, 0.5, 0.75).
-
-% population( initial_number_of_entities, type_of_entity, number_of_days_to_simulate, final_number_of_entities)
-population(N, _, 0, N). % Without advancing any days initial = final
-population(N, Entity, D1, F) :-  N > 0, D1 > 0, entity_traits(Entity, Birth, Death), I is N + N*Birth - N*Death, D2 is D1-1, population(I, Entity, D2, F).
-% -----------------------------------------------------------------------------------------
-
-
-% geography(Biome, Temp, Rain, Food)
-%   Biome is the biome identifier
-%   Temp is the relative temperature: hot, normal, cold
-%   Rain is the humidity/rainfall: humid, normal, dry
-%   Food is the food availability: plentiful, normal, scarce
-geography(desert, hot, dry, scarce)
-geography(jungle, hot, humid, plentiful)
-geography(plains, normal, normal, normal)
-
+% Basic Natural Selection Functionality ------------------------------------------------------------------------
 % entity(Species, PS, PR)
 %   Species is the type of species
 %   PS is the probability that the entity survives
