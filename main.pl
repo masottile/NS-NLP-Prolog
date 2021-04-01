953d8579dfa7af1e720806acdd422ba613aef05d
Maria Sottile <31717619+masottile@users.noreply.github.com>
2021-04-01 16:51:45 -0700

    fixed accidental errors in code (non optimal solution still wip)

--- main.pl
+++ main.pl
@@ -17,8 +17,28 @@ entity(blob, 0.75, 0.5).
 
 pop1day(0,_,_,_,F,F).
 % case that individual dies
-pop1day(N1, Species, false, _ , T1, F) :- N1 > 0, N2 is N1-1, T2 is T1, entity(Species, PR, PS), pop1day(N2, Species, random_float() < PS, random_float() < PR, T2, F).
+pop1day(N1, Species, RNS, _ , T1, F) :-
+    N1 > 0,
+    entity(Species, PS, _),
+    RNS > PS, % This individual does not survive
+    N2 is N1-1, 
+    T2 is T1, 
+    pop1day(N2, Species, random_float(), random_float(), T2, F).
 % case individual survives but doesn't reproduce
-pop1day(N1, Species, true, false , T1, F) :- N1 > 0, N2 is N1-1, T2 is T1+1, entity(Species, PR, PS), pop1day(N2, Species, random_float() < PS, random_float() < PR, T2, F).
+pop1day(N1, Species, RNS, RNR , T1, F) :- 
+    N1 > 0,
+    entity(Species, PS, PR),
+    RNS < PS, % This individual survives
+    RNR > PR, % This individual does not reproduce
+    N2 is N1-1, 
+    T2 is T1+1, 
+    pop1day(N2, Species, random_float(), random_float(), T2, F).
 % case individual survives and reproduces
-pop1day(N1, Species, true, true , T1, F) :- N1 > 0, N2 is N1-1, T2 is T1+2, entity(Species, PR, PS), pop1day(N2, Species, random_float() < PS, random_float() < PR, T2, F).
+pop1day(N1, Species, RNS, RNR , T1, F) :- 
+    N1 > 0,
+    entity(Species, PS, PR),
+    RNS < PS, % This individual survives
+    RNR < PR, % This individual reproduce
+    N2 is N1-1, 
+    T2 is T1+2, 
+    pop1day(N2, Species, random_float(), random_float(), T2, F).
